3e94b03f5afe784d7c06e576da4640239b236dc7
PDillis <daporres@ufm.edu>
2023-04-17 02:26:27 +0200

    linear warmup, cosine cooldown done; setup experiments; model parameters for special tokens; code linting

--- dataloaders/__init__.py
+++ dataloaders/__init__.py
@@ -5,7 +5,11 @@ from torch.utils.data import DataLoader
 
 def make_data_loader(model_name, base_dir, train_dataset_names, batch_size, valid_dataset_names, batch_size_eval):
     train_set = carlaImages.carlaImages(model_name, base_dir, train_dataset_names, split='train')
-    train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True, num_workers=g_conf.NUM_WORKER, drop_last=True)  # we drop the last non-full batch to avoid error
+    train_loader = DataLoader(train_set,
+                              batch_size=batch_size,
+                              shuffle=True,
+                              num_workers=g_conf.NUM_WORKER,
+                              drop_last=True)  # we drop the last non-full batch to avoid error
     val_loaders_list=[]
     for valid_dataset_name in valid_dataset_names:
         val_set = carlaImages.carlaImages(model_name, base_dir, [valid_dataset_name], split='val')
