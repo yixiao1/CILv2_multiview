70ac13af9e224b1fe97bab980f642cacc93dd171
PDillis <daporres@ufm.edu>
2023-05-12 02:44:33 +0200

    Setup one sequence (concatenate 3 images) and base configs

--- network/models/architectures/CIL_multiview/CIL_multiview_vit_oneseq.py
+++ network/models/architectures/CIL_multiview/CIL_multiview_vit_oneseq.py
@@ -3,7 +3,6 @@ import torch.nn as nn
 import importlib
 
 from configs import g_conf
-from network.models.building_blocks import FC
 from network.models.building_blocks.PositionalEncoding import PositionalEncoding
 
 from einops import rearrange
@@ -17,52 +16,59 @@ class CIL_multiview_vit_oneseq(nn.Module):
 
         # Get ViT model characteristics (our new perception module)
         vit_module = importlib.import_module('network.models.building_blocks.vit')
-        vit_module = getattr(vit_module, params['encoder_embedding']['perception']['vit']['name'])
+        try:
+            module_name = self.params['encoder_embedding']['perception']['vit']['name']
+        except KeyError:
+            module_name = self.params['encoder_embedding']['perception']['res']['name']
+        vit_module = getattr(vit_module, module_name)
         self.encoder_embedding_perception = vit_module(pretrained=g_conf.IMAGENET_PRE_TRAINED)
 
-        # Network pieces
-        self.tfx_class_token = self.encoder_embedding_perception.class_token  # [1, 1, D], D is the hidden dimension
-        self.tfx_conv_projection = self.encoder_embedding_perception.conv_proj
-        self.tfx_encoder = self.encoder_embedding_perception.encoder
+        self.image_channels, self.image_height, self.image_width = g_conf.IMAGE_SHAPE
 
         # Get the vision transformer characteristics
         self.tfx_hidden_dim = self.encoder_embedding_perception.hidden_dim  # D
         self.tfx_patch_size = self.encoder_embedding_perception.patch_size  # P
-        self.tfx_image_size = self.encoder_embedding_perception.image_size  # H, W
+        self.tfx_num_patches_h = self.image_height // self.tfx_patch_size  # H/P
+        self.tfx_num_patches_w = self.image_width // self.tfx_patch_size  # W/P
+        self.tfx_num_patches = self.tfx_num_patches_h * self.tfx_num_patches_w  # (H/P)*(W/P)
 
-        # Token characteristics from ViT pre-trained model
-        old_seq_length = self.encoder_embedding_perception.seq_length  # (H//P)^2 + 1
-        old_patch_number = old_seq_length - 1  # (H//P)^2
-        old_seq_length_1d = int(old_patch_number ** 0.5)  # H//P; keep same names as in official code
+        # Network pieces
+        self.tfx_conv_projection = self.encoder_embedding_perception.conv_proj
+        self.tfx_encoder = self.encoder_embedding_perception.encoder
 
-        # Check image size and patch size
-        assert g_conf.IMAGE_SHAPE[1] % self.tfx_patch_size == 0, 'Image height must be divisible by patch size!'
-        assert g_conf.IMAGE_SHAPE[2] % self.tfx_patch_size == 0, 'Image width must be divisible by patch size!'
+        self.command = nn.Linear(g_conf.DATA_COMMAND_CLASS_NUM, self.tfx_hidden_dim)
+        self.speed = nn.Linear(1, self.tfx_hidden_dim)
 
-        # Get the sequence length w.r.t. the image shape and patch size
-        new_seq_length_h = g_conf.IMAGE_SHAPE[1] // self.tfx_patch_size  # H//P
-        new_seq_length_w = g_conf.IMAGE_SHAPE[2] // self.tfx_patch_size  # W//P
-        new_seq_length = new_seq_length_h * new_seq_length_w + 1  # (H//P)*(W//P) + 1
+        # Additional tokens
+        self.tfx_class_token = self.encoder_embedding_perception.class_token
+        if g_conf.PRETRAINED_ACC_STR_TOKENS:
+            # Start from the [CLS] token of the pretrained model
+            print('Initializing the acceleration ([ACC]) and steering ([STR]) tokens with the [CLS] token...')
+            self.tfx_accel_token = nn.Parameter(self.tfx_class_token.detach().clone())
+            self.tfx_steer_token = nn.Parameter(self.tfx_class_token.detach().clone())
+        else:
+            # Start from scratch
+            print('Randomly initializing the acceleration ([ACC]) and steering ([STR]) tokens...')
+            self.tfx_accel_token = nn.Parameter(torch.empty(1, 1, self.tfx_hidden_dim).normal_(std=0.02))
+            self.tfx_steer_token = nn.Parameter(torch.empty(1, 1, self.tfx_hidden_dim).normal_(std=0.02))
 
-        # Interpolate the positional encoding only if the sequence length changed
-        if new_seq_length != old_seq_length:
-            print(f'WARNING: Sequence length changed from {old_seq_length} to {new_seq_length}!')
-            self.tfx_patch_number = int(g_conf.ENCODER_INPUT_FRAMES_NUM) * len(g_conf.DATA_USED) * old_patch_number  # S*cam*(H//P)^2
-            new_seq_shape = (int(g_conf.ENCODER_INPUT_FRAMES_NUM) * len(g_conf.DATA_USED) * old_seq_length_1d,
-                             old_seq_length_1d)  # [S*cam*H//P, H//P]
-            # Interpolate the positional encoding to the correct size
-            self.interpolate_pos_encoding(new_seq_shape)
+        self.tfx_seq_length = int(g_conf.ENCODER_INPUT_FRAMES_NUM) * len(g_conf.DATA_USED) * self.tfx_num_patches + 3  # K=3 for the additional tokens
+        self.act_tokens_pos = [0, 1]  # Which tokens in the sequence to use for the action
 
-        join_dim = self.tfx_hidden_dim  # params['TxEncoder']['d_model']
+        # Remove the [CLS] token if needed
+        if g_conf.REMOVE_CLS_TOKEN:
+            print('Removing the [CLS] token from the sequence...')
+            self.tfx_seq_length -= 1  # K=2
+            del self.tfx_class_token
 
-        self.command = nn.Linear(g_conf.DATA_COMMAND_CLASS_NUM, self.tfx_hidden_dim)#  params['TxEncoder']['d_model'])
-        self.speed = nn.Linear(1, self.tfx_hidden_dim)  # params['TxEncoder']['d_model'])
+        # Add the command and speed as tokens to the sequence
+        if g_conf.CMD_SPD_TOKENS:
+            self.tfx_seq_length += 2
+            self.act_tokens_pos = [_ + 2 for _ in self.act_tokens_pos]
 
-        self.action_output = FC(params={'neurons': [join_dim] +
-                                            params['action_output']['fc']['neurons'] +
-                                            [len(g_conf.TARGETS)],
-                                 'dropouts': params['action_output']['fc']['dropouts'] + [0.0],
-                                 'end_layer': True})
+        # Positional Encoding (fixed for now)
+        del self.tfx_encoder.pos_embedding  # A nn.Parameter, so it most go
+        self.pos_embedding = PositionalEncoding(d_model=self.tfx_hidden_dim, max_len=self.tfx_seq_length)
 
         # We don't want to undo the pretrained weights of the ViT!
         for name, module in self.named_modules():
@@ -100,6 +106,44 @@ class CIL_multiview_vit_oneseq(nn.Module):
                 torch.empty(1, self.tfx_patch_number + 1, self.tfx_hidden_dim).normal_(std=0.02)
             )
 
+    def encode_observations(self, s, s_d, s_s):
+        S = int(g_conf.ENCODER_INPUT_FRAMES_NUM)  # Number of frames per camera in sequence
+        B = s_d[0].shape[0]  # Batch size
+        cam = len(g_conf.DATA_USED)  # Number of cameras/views
+
+        x = torch.stack([torch.stack(s[i], dim=1) for i in range(S)], dim=1)  # [B, S, cam, 3, H, W]
+        x = rearrange(x, 'B S cam C H W -> (B S cam) C H W')  # [B*S*cam, C=3, H, W]
+        d = s_d[-1]  # [B, 4]
+        s = s_s[-1]  # [B, 1]
+
+        # image embedding
+        # First, patch and embed the input images
+        e_p = self.tfx_conv_projection(x)  # [B*S*cam, D, H/P, W/P]; usually H = W = 224
+        e_p = rearrange(e_p, '(B S cam) D patches_H patches_W -> B (S cam patches_H patches_W) D',
+                        B=B, cam=cam, D=self.tfx_hidden_dim)  # [B, S*cam*H*W/P^2, D]
+
+        # Setup the first tokens in the sequence
+        n = e_p.shape[0]  # B
+        first_tokens = [self.tfx_steer_token.expand(n, -1, -1),
+                        self.tfx_accel_token.expand(n, -1, -1)][::-2 * g_conf.OLD_TOKEN_ORDER + 1]
+        if not g_conf.REMOVE_CLS_TOKEN:
+            first_tokens = [*first_tokens, self.tfx_class_token.expand(n, -1, -1)]
+
+        # Concatenate the first tokens to the image embeddings
+        e_p = torch.cat([*first_tokens, e_p], dim=1)  # [B, S*cam*H*W/P^2 + K, D]]), K = 3 if w/CLS token else 2
+
+        # Embedding of command and speed
+        e_d = self.command(d).unsqueeze(1)  # [B, 1, D]
+        e_s = self.speed(s).unsqueeze(1)  # [B, 1, D]
+
+        # Add the embeddings to the image embeddings
+        if g_conf.CMD_SPD_TOKENS:
+            encoded_obs = torch.cat([e_p, e_d, e_s], dim=1)  # [B, S*cam*H*W/P^2 + K, D]; K = 5 w/CLS token else 4
+        else:
+            encoded_obs = e_p + e_d + e_s  # [B, S*cam*H*W/P^2 + K), D]; K = 3 w/CLS token else 2
+
+        return encoded_obs
+
     def forward(self, s, s_d, s_s):
         """
         Arguments:
@@ -112,37 +156,20 @@ class CIL_multiview_vit_oneseq(nn.Module):
         B = s_d[0].shape[0]     # Batch size; will change if using multiple GPUs
         cam = len(g_conf.DATA_USED)  # Number of cameras
 
-        # Image info
-        C, H, W = g_conf.IMAGE_SHAPE
+        encoded_obs = self.encode_observations(s, s_d, s_s)  # [B, S*cam*H*W/P^2 + K, D]
 
-        x = torch.stack([torch.stack(s[i], dim=1) for i in range(S)], dim=1) # [B, S, cam, 3, H, W]
-        x = x.view(B*S*cam, C, H, W)  # [B*S*cam, 3, H, W]
-        d = s_d[-1]  # [B, 4]
-        s = s_s[-1]  # [B, 1]
-
-        # image embedding
-        # First, patch and embed all the input images
-        e_p = self.tfx_conv_projection(x)  # [B*S*cam, D, H//P, W//P]
-        e_p = rearrange(e_p, '(batch S cam) D patches_H patches_W -> batch (S cam patches_H patches_W) D', 
-                        S=S, cam=cam, D=self.tfx_hidden_dim)  # [B, S*cam*((H//P)^2), D]
-
-        # Now the rest of forward
-        e_p = torch.cat([self.tfx_class_token.expand(B, -1, -1), e_p], dim=1)  # [B, S*cam*(H//P)^2 + 1, D]
-
-        # Embedding of command and speed
-        e_d = self.command(d).unsqueeze(1)     # [B, 1, D]
-        e_s = self.speed(s).unsqueeze(1)       # [B, 1, D]
-
-        # Add the embeddings to the image embeddings (TODO: try different ways to do this)
-        encoded_obs = e_p + e_d + e_s  # [B, S*cam*(H//P)^2 + 1, D]]
+        # Add positional encoding
+        encoded_obs = self.pos_embedding(encoded_obs)  # [B, S*cam*H*W/P^2 + K, D]]
 
         # Pass on to the Transformer encoder
-        in_memory = self.tfx_encoder(encoded_obs)  # [B, S*cam*(H//P)^2 + 1, D]]
-        # Use only the [CLS] token for the action prediction 
-        in_memory = in_memory[:, 0, :]  # [B, S*cam*(H//P)^2 + 1, D] => [B, D]
+        in_memory = self.tfx_encoder(encoded_obs, pre_pos_embed=True)  # [B, S*cam*H*W/P^2 + K, D]
+
+        # Action prediction: use only the action tokens for the prediction
+        in_memory = in_memory[:, self.act_tokens_pos]  # [B, S*cam*H*W/P^2 + K, D] => [B, t, D]; t = 2
+        in_memory = rearrange(in_memory, 'B t D -> B D t')  # [B, t, D] => [B, D, t]
 
         # Action prediction
-        action_output = self.action_output(in_memory).unsqueeze(1)  # (B, D) -> (B, 1, len(TARGETS))
+        action_output = torch.mean(in_memory, dim=1, keepdim=True)  # [B, D, t] -> (B, 1, len(TARGETS))
 
         return action_output
 
@@ -151,39 +178,23 @@ class CIL_multiview_vit_oneseq(nn.Module):
         B = s_d[0].shape[0]  # Batch size; will change if using multiple GPUs
         cam = len(g_conf.DATA_USED)  # Number of cameras
 
-        # Image info
-        C, H, W = g_conf.IMAGE_SHAPE
+        encoded_obs = self.encode_observations(s, s_d, s_s)  # [B, S*cam*H*W/P^2 + K, D]
 
-        x = torch.stack([torch.stack(s[i], dim=1) for i in range(S)], dim=1)  # [B, S, cam, 3, H, W]
-        x = x.view(B * S * cam, C, H, W)  # [B*S*cam, 3, H, W]
-        d = s_d[-1]  # [B, 4]
-        s = s_s[-1]  # [B, 1]
-
-        # image embedding
-        # First, patch and embed all the input images
-        e_p = self.tfx_conv_projection(x)  # [B*S*cam, D, H//P, W//P]
-        e_p = rearrange(e_p, '(batch S cam) D patches_H patches_W -> batch (S cam patches_H patches_W) D',
-                        S=S, cam=cam, D=self.tfx_hidden_dim)  # [B, S*cam*((H//P)^2), D]
-
-        # Now the rest of forward
-        e_p = torch.cat([self.tfx_class_token.expand(B, -1, -1), e_p], dim=1)  # [B, S*cam*(H//P)^2 + 1, D]
-
-        # Embedding of command and speed
-        e_d = self.command(d).unsqueeze(1)  # [B, 1, D]
-        e_s = self.speed(s).unsqueeze(1)  # [B, 1, D]
-
-        # Add the embeddings to the image embeddings (TODO: try different ways to do this)
-        encoded_obs = e_p + e_d + e_s  # [B, S*cam*(H//P)^2 + 1, D]]
+        # Add positional encoding
+        encoded_obs = self.pos_embedding(encoded_obs)  # [B, S*cam*H*W/P^2 + K, D]]
 
         # Pass on to the Transformer encoder
-        in_memory = self.tfx_encoder(encoded_obs)  # [B, S*cam*(H//P)^2 + 1, D]]
-        # Use only the [CLS] token for the action prediction
-        in_memory = in_memory[:, 0, :]  # [B, S*cam*(H//P)^2 + 1, D] => [B, D]
+        # [B, S*cam*H*W/P^2 + K, D], num_layers * [B, S*cam*H*W/P^2 + K, S*cam*H*W/P^2 + K]
+        in_memory, attn_weights = self.tfx_encoder.forward_return_attn(encoded_obs, pre_pos_embed=True)
+
+        # Action prediction: use only the action tokens for the prediction
+        in_memory = in_memory[:, self.act_tokens_pos]  # [B, S*cam*H*W/P^2 + K, D] => [B, t, D]; t = 2
+        in_memory = rearrange(in_memory, 'B t D -> B D t')  # [B, t, D] => [B, D, t]
 
         # Action prediction
-        action_output = self.action_output(in_memory).unsqueeze(1)  # (B, D) -> (B, 1, len(TARGETS))
+        action_output = torch.mean(in_memory, dim=1, keepdim=True)  # [B, D, t] -> (B, 1, len(TARGETS))
 
-        return action_output  # TODO: return attn_weights?
+        return action_output
 
     def generate_square_subsequent_mask(self, sz):
         r"""Generate a square mask for the sequence. The masked positions are filled with float('-inf').
