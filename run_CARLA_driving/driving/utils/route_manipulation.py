8dd5e04191c390cab46ef27312f399a6bc5b19e7
yixiao1 <yixiaoyixiao123@gmail.com>
2023-12-06 07:02:56 +0000

    fix a carla import bug

--- run_CARLA_driving/driving/utils/route_manipulation.py
+++ run_CARLA_driving/driving/utils/route_manipulation.py
@@ -140,9 +140,10 @@ def interpolate_trajectory(world, waypoints_trajectory, hop_resolution=1.0):
         - hop_resolution: is the resolution, how dense is the provided trajectory going to be made
     """
 
-    dao = GlobalRoutePlannerDAO(world.get_map(), hop_resolution)
-    grp = GlobalRoutePlanner(dao)
-    grp.setup()
+    #dao = GlobalRoutePlannerDAO(world.get_map(), hop_resolution)
+    #grp = GlobalRoutePlanner(dao)
+    #grp.setup()
+    grp = GlobalRoutePlanner(world.get_map(), hop_resolution)
     # Obtain route plan
     route = []
     for i in range(len(waypoints_trajectory) - 1):   # Goes until the one before the last.
