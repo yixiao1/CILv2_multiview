Train with different gaze predictions, filter out ticks that don't have all the sensors and CAN buses, fix validation Att Loss calculation, we can load RGB images named other than rgb and with multiple suffixes (png, jpg, etc.)

_utils/evaluation.py
@@ -185,13 +185,21 @@ def evaluation_on_model(model: nn.Module,
                         
                         
                     elif g_conf.ATTENTION_TYPE == 'human_gaze':
-                        src_atts = [data['current'][i]['gaze_pred'].cuda() for i in range(len(data['current']))]  # [B, 1, h, 3*w]
+                        # Human gaze can be gaze_pred, scout14ep1, or scout15ep2
+                        gaze_keys = ['gaze_pred', 'scout14ep1', 'scout15ep2']
+                        available_key = utils.get_available_key(data['current'][0], gaze_keys)
+                        
+                        src_atts = [data['current'][i][available_key].cuda() for i in range(len(data['current']))]  # [B, 1, h, 3*w]
                         # Split the attention into left, central, and right
                         src_atts_left = [src_atts[i][:, :, :, :model.resize_att_w] for i in range(len(src_atts))]
                         src_atts_central = [src_atts[i][:, :, :, model.resize_att_w:2*model.resize_att_w] for i in range(len(src_atts))]
                         src_atts_right = [src_atts[i][:, :, :, 2*model.resize_att_w:] for i in range(len(src_atts))]
                     elif g_conf.ATTENTION_TYPE == 'human_gaze_semantic':
-                        src_atts = [data['current'][i]['gaze_pred'].cuda() for i in range(len(data['current']))]
+                        # Human gaze can be gaze_pred, scout14ep1, or scout15ep2
+                        gaze_keys = ['gaze_pred', 'scout14ep1', 'scout15ep2']
+                        available_key = utils.get_available_key(data['current'][0], gaze_keys)
+                        
+                        src_atts = [data['current'][i][available_key].cuda() for i in range(len(data['current']))]  # [B, 1, h, 3*w]
                         # Split the attention into left, central, and right
                         src_atts_left = [src_atts[i][:, :, :, :model.resize_att_w] for i in range(len(src_atts))]
                         src_atts_central = [src_atts[i][:, :, :, model.resize_att_w:2*model.resize_att_w] for i in range(len(src_atts))]
@@ -208,7 +216,11 @@ def evaluation_on_model(model: nn.Module,
                         src_atts_right = [torch.max(src_atts_right[0], vrt_atts_right[0])]
                     
                     elif g_conf.ATTENTION_TYPE == 'human_gaze_semantic_sum':
-                        src_atts = [data['current'][i]['gaze_pred'].cuda() for i in range(len(data['current']))]
+                        # Human gaze can be gaze_pred, scout14ep1, or scout15ep2
+                        gaze_keys = ['gaze_pred', 'scout14ep1', 'scout15ep2']
+                        available_key = utils.get_available_key(data['current'][0], gaze_keys)
+                        
+                        src_atts = [data['current'][i][available_key].cuda() for i in range(len(data['current']))]  # [B, 1, h, 3*w]
                         # Split the attention into left, central, and right
                         src_atts_left = [src_atts[i][:, :, :, :model.resize_att_w] for i in range(len(src_atts))]
                         src_atts_central = [src_atts[i][:, :, :, model.resize_att_w:2*model.resize_att_w] for i in range(len(src_atts))]
@@ -256,7 +268,7 @@ def evaluation_on_model(model: nn.Module,
                             for j in range(len(src_images[i])):
                                 src_images[i][j] = src_images[i][j] * src_attn_masks[i][j]
 
-                action_outputs, resnet_inter, att_out = model.forward_eval(src_images, src_directions, src_speeds)
+                action_outputs, resnet_inter, att_out = model.forward(src_images, src_directions, src_speeds)
                 torch.cuda.synchronize()
                 if g_conf.EARLY_ATTENTION:
                     resnet_inter = resnet_inter[g_conf.RN_ATTENTION_LAYER]
@@ -371,13 +383,21 @@ def evaluation_on_model(model: nn.Module,
                             eval_atts_right = [uniform_mask]
                         
                         elif g_conf.ATTENTION_TYPE == 'human_gaze':
-                            src_atts = [data['current'][i]['gaze_pred'].cuda() for i in range(len(data['current']))]  # [B, 1, h, 3*w]
+                            # Human gaze can be gaze_pred, scout14ep1, or scout15ep2
+                            gaze_keys = ['gaze_pred', 'scout14ep1', 'scout15ep2']
+                            available_key = utils.get_available_key(data['current'][0], gaze_keys)
+                            
+                            src_atts = [data['current'][i][available_key].cuda() for i in range(len(data['current']))]  # [B, 1, h, 3*w]
                             # Split the attention into left, central, and right
                             eval_atts_left = [src_atts[i][:, :, :, :model.resize_att_w] for i in range(len(src_atts))]
                             eval_atts_central = [src_atts[i][:, :, :, model.resize_att_w:2*model.resize_att_w] for i in range(len(src_atts))]
                             eval_atts_right = [src_atts[i][:, :, :, 2*model.resize_att_w:] for i in range(len(src_atts))]
                         elif g_conf.ATTENTION_TYPE == 'human_gaze_semantic':
-                            src_atts = [data['current'][i]['gaze_pred'].cuda() for i in range(len(data['current']))]
+                            # Human gaze can be gaze_pred, scout14ep1, or scout15ep2
+                            gaze_keys = ['gaze_pred', 'scout14ep1', 'scout15ep2']
+                            available_key = utils.get_available_key(data['current'][0], gaze_keys)
+                            
+                            src_atts = [data['current'][i][available_key].cuda() for i in range(len(data['current']))]  # [B, 1, h, 3*w]
                             # Split the attention into left, central, and right
                             eval_atts_left = [src_atts[i][:, :, :, :model.resize_att_w] for i in range(len(src_atts))]
                             eval_atts_central = [src_atts[i][:, :, :, model.resize_att_w:2*model.resize_att_w] for i in range(len(src_atts))]
@@ -394,7 +414,11 @@ def evaluation_on_model(model: nn.Module,
                             eval_atts_right = [torch.max(eval_atts_right[0], vrt_atts_right[0])]
                             
                         elif g_conf.ATTENTION_TYPE == 'human_gaze_semantic_sum':
-                            src_atts = [data['current'][i]['gaze_pred'].cuda() for i in range(len(data['current']))]
+                            # Human gaze can be gaze_pred, scout14ep1, or scout15ep2
+                            gaze_keys = ['gaze_pred', 'scout14ep1', 'scout15ep2']
+                            available_key = utils.get_available_key(data['current'][0], gaze_keys)
+                            
+                            src_atts = [data['current'][i][available_key].cuda() for i in range(len(data['current']))]  # [B, 1, h, 3*w]
                             # Split the attention into left, central, and right
                             eval_atts_left = [src_atts[i][:, :, :, :model.resize_att_w] for i in range(len(src_atts))]
                             eval_atts_central = [src_atts[i][:, :, :, model.resize_att_w:2*model.resize_att_w] for i in range(len(src_atts))]

console/train_val.py
@@ -222,13 +222,21 @@ def train_upstream_task(model, optimizer, rank=0, world_size=1):
                         src_atts_right = [uniform_mask]
 
                     elif g_conf.ATTENTION_TYPE == 'human_gaze':
-                        src_atts = [data['current'][i]['gaze_pred'].to(f'cuda:{model.device_ids[0]}') for i in range(len(data['current']))]  # [B, 1, h, 3*w]
+                        # Human gaze can be gaze_pred, scout14ep1, or scout15ep2
+                        gaze_keys = ['gaze_pred', 'scout14ep1', 'scout15ep2']
+                        available_key = utils.get_available_key(data['current'][0], gaze_keys)
+                        
+                        src_atts = [data['current'][i][available_key].to(f'cuda:{model.device_ids[0]}') for i in range(len(data['current']))]  # [B, 1, h, 3*w]
                         # Split the attention into left, central, and right
                         src_atts_left = [src_atts[i][:, :, :, :model.resize_att_w] for i in range(len(src_atts))]
                         src_atts_central = [src_atts[i][:, :, :, model.resize_att_w:2*model.resize_att_w] for i in range(len(src_atts))]
                         src_atts_right = [src_atts[i][:, :, :, 2*model.resize_att_w:] for i in range(len(src_atts))]
                     elif g_conf.ATTENTION_TYPE == 'human_gaze_semantic':
-                        src_atts = [data['current'][i]['gaze_pred'].to(f'cuda:{model.device_ids[0]}') for i in range(len(data['current']))]
+                        # Human gaze can be gaze_pred, scout14ep1, or scout15ep2
+                        gaze_keys = ['gaze_pred', 'scout14ep1', 'scout15ep2']
+                        available_key = utils.get_available_key(data['current'][0], gaze_keys)
+                        
+                        src_atts = [data['current'][i][available_key].to(f'cuda:{model.device_ids[0]}') for i in range(len(data['current']))]  # [B, 1, h, 3*w]
                         # Split the attention into left, central, and right
                         src_atts_left = [src_atts[i][:, :, :, :model.resize_att_w] for i in range(len(src_atts))]
                         src_atts_central = [src_atts[i][:, :, :, model.resize_att_w:2*model.resize_att_w] for i in range(len(src_atts))]
@@ -245,7 +253,11 @@ def train_upstream_task(model, optimizer, rank=0, world_size=1):
                         src_atts_right = [torch.max(src_atts_right[0], vrt_atts_right[0])]
                         
                     elif g_conf.ATTENTION_TYPE == 'human_gaze_semantic_sum':
-                        src_atts = [data['current'][i]['gaze_pred'].to(f'cuda:{model.device_ids[0]}') for i in range(len(data['current']))]
+                        # Human gaze can be gaze_pred, scout14ep1, or scout15ep2
+                        gaze_keys = ['gaze_pred', 'scout14ep1', 'scout15ep2']
+                        available_key = utils.get_available_key(data['current'][0], gaze_keys)
+                        
+                        src_atts = [data['current'][i][available_key].to(f'cuda:{model.device_ids[0]}') for i in range(len(data['current']))]  # [B, 1, h, 3*w]
                         # Split the attention into left, central, and right
                         src_atts_left = [src_atts[i][:, :, :, :model.resize_att_w] for i in range(len(src_atts))]
                         src_atts_central = [src_atts[i][:, :, :, model.resize_att_w:2*model.resize_att_w] for i in range(len(src_atts))]
@@ -416,13 +428,21 @@ def train_upstream_task(model, optimizer, rank=0, world_size=1):
                         src_atts_right = [uniform_mask]
 
                     elif g_conf.ATTENTION_TYPE == 'human_gaze':
-                        src_atts = [data['current'][i]['gaze_pred'].cuda() for i in range(len(data['current']))]  # [B, 1, h, 3*w]
+                        # Human gaze can be gaze_pred, scout14ep1, or scout15ep2
+                        gaze_keys = ['gaze_pred', 'scout14ep1', 'scout15ep2']
+                        available_key = utils.get_available_key(data['current'][0], gaze_keys)
+                        
+                        src_atts = [data['current'][i][available_key].cuda() for i in range(len(data['current']))]  # [B, 1, h, 3*w]
                         # Split the attention into left, central, and right
                         src_atts_left = [src_atts[i][:, :, :, :model.resize_att_w] for i in range(len(src_atts))]
                         src_atts_central = [src_atts[i][:, :, :, model.resize_att_w:2*model.resize_att_w] for i in range(len(src_atts))]
                         src_atts_right = [src_atts[i][:, :, :, 2*model.resize_att_w:] for i in range(len(src_atts))]
                     elif g_conf.ATTENTION_TYPE == 'human_gaze_semantic':
-                        src_atts = [data['current'][i]['gaze_pred'].cuda() for i in range(len(data['current']))]
+                        # Human gaze can be gaze_pred, scout14ep1, or scout15ep2
+                        gaze_keys = ['gaze_pred', 'scout14ep1', 'scout15ep2']
+                        available_key = utils.get_available_key(data['current'][0], gaze_keys)
+                        
+                        src_atts = [data['current'][i][available_key].cuda() for i in range(len(data['current']))]  # [B, 1, h, 3*w]
                         # Split the attention into left, central, and right
                         src_atts_left = [src_atts[i][:, :, :, :model.resize_att_w] for i in range(len(src_atts))]
                         src_atts_central = [src_atts[i][:, :, :, model.resize_att_w:2*model.resize_att_w] for i in range(len(src_atts))]
@@ -439,7 +459,11 @@ def train_upstream_task(model, optimizer, rank=0, world_size=1):
                         src_atts_right = [torch.max(src_atts_right[0], vrt_atts_right[0])]
                         
                     elif g_conf.ATTENTION_TYPE == 'human_gaze_semantic_sum':
-                        src_atts = [data['current'][i]['gaze_pred'].cuda() for i in range(len(data['current']))]
+                        # Human gaze can be gaze_pred, scout14ep1, or scout15ep2
+                        gaze_keys = ['gaze_pred', 'scout14ep1', 'scout15ep2']
+                        available_key = utils.get_available_key(data['current'][0], gaze_keys)
+                        
+                        src_atts = [data['current'][i][available_key].cuda() for i in range(len(data['current']))]  # [B, 1, h, 3*w]
                         # Split the attention into left, central, and right
                         src_atts_left = [src_atts[i][:, :, :, :model.resize_att_w] for i in range(len(src_atts))]
                         src_atts_central = [src_atts[i][:, :, :, model.resize_att_w:2*model.resize_att_w] for i in range(len(src_atts))]

dataloaders/transforms.py
@@ -8,7 +8,8 @@ import PIL
 from configs import g_conf
 
 
-def canbus_normalization(can_bus_dict, data_ranges, ted_normalization: bool = False):
+def canbus_normalization(can_bus_dict, data_ranges, ted_normalization: bool = False,
+                         shift_command: int = 0):
     for dtype in data_ranges.keys():
         # we normalize the steering in range of [-1.0, 1.0]
         if dtype in ['steer', 'acceleration']:
@@ -34,9 +35,9 @@ def canbus_normalization(can_bus_dict, data_ranges, ted_normalization: bool = Fa
         # we encode directions to one-hot vector
         if g_conf.DATA_COMMAND_ONE_HOT:
             if g_conf.DATA_COMMAND_CLASS_NUM == 4:
-                can_bus_dict['direction'] = encode_directions_4(can_bus_dict['direction'])
+                can_bus_dict['direction'] = encode_directions_4(can_bus_dict['direction'], shift_command=shift_command)
             elif g_conf.DATA_COMMAND_CLASS_NUM == 6:
-                can_bus_dict['direction'] = encode_directions_6(can_bus_dict['direction'])
+                can_bus_dict['direction'] = encode_directions_6(can_bus_dict['direction'], shift_command=shift_command)
         else:
             # we remark directions from 1-4 to 0-3 for torch.embedding layer
             can_bus_dict['direction'] = [can_bus_dict['direction']-1]
@@ -48,7 +49,7 @@ def train_transform(data: dict, image_shape: 'tuple[int]', resize_attention: 'tu
         output is from 0-1 float.
     """
     for camera_type in g_conf.DATA_USED:
-        if 'rgb' in camera_type:
+        if any(cam_type in camera_type for cam_type in ['rgb', 'sekonix', 'conti']):
             image = data[camera_type]
             image = image.resize((image_shape[2], image_shape[1]))  # Note: Bicubic interpolation by default
             # Augmentations, if used
@@ -85,7 +86,7 @@ def train_transform(data: dict, image_shape: 'tuple[int]', resize_attention: 'tu
             if g_conf.ATTENTION_AS_NEW_CHANNEL:
                 image = TF.normalize(image, [0.5], [0.5])
             data[camera_type] = image
-        elif 'gaze_pred' in camera_type and not g_conf.ATTENTION_AS_INPUT:
+        elif any(gaze_type in camera_type for gaze_type in ['gaze_pred', 'scout']) and not g_conf.ATTENTION_AS_INPUT:
             image = data[camera_type]
             image = cv2.resize(np.array(image), (3 * resize_attention[0], resize_attention[1]), interpolation=cv2.INTER_AREA)
             image = TF.to_tensor(image)
@@ -97,7 +98,7 @@ def train_transform(data: dict, image_shape: 'tuple[int]', resize_attention: 'tu
 
 def val_transform(data, image_shape, resize_attention: 'tuple[int]' = (13, 8)):
     for camera_type in g_conf.DATA_USED:
-        if 'rgb' in camera_type:
+        if any(cam_type in camera_type for cam_type in ['rgb', 'sekonix', 'conti']):
             image = data[camera_type]
             ## WE ALREADY PRE-PROCESSED IMAGES TO DESIRED SIZE
             height = image_shape[1]
@@ -122,7 +123,7 @@ def val_transform(data, image_shape, resize_attention: 'tuple[int]' = (13, 8)):
             if g_conf.ATTENTION_AS_NEW_CHANNEL:
                 image = TF.normalize(image, [0.5], [0.5])
             data[camera_type] = image
-        elif 'gaze_pred' in camera_type and not g_conf.ATTENTION_AS_INPUT:
+        elif any(gaze_type in camera_type for gaze_type in ['gaze_pred', 'scout']) and not g_conf.ATTENTION_AS_INPUT:
             image = data[camera_type]
             image = cv2.resize(np.array(image), (3 * resize_attention[0], resize_attention[1]), interpolation=cv2.INTER_AREA)
             image = TF.to_tensor(image)
@@ -138,7 +139,7 @@ def ted_transform(data: dict, image_shape: 'tuple[int]', resize_attention: 'tupl
     # Set the default sensors from the config if none is specified
     sensors_used = g_conf.DATA_USED if sensors_used is None else sensors_used
     for camera_type in sensors_used:
-        if 'rgb' in camera_type:
+        if any(cam_type in camera_type for cam_type in ['rgb', 'sekonix', 'conti']):
             image = data[camera_type]
             image = image.resize((image_shape[2], image_shape[1]))  # Note: Bicubic interpolation by default
             # Augmentations, if used
@@ -175,7 +176,7 @@ def ted_transform(data: dict, image_shape: 'tuple[int]', resize_attention: 'tupl
             if g_conf.ATTENTION_AS_NEW_CHANNEL:
                 image = TF.normalize(image, [0.5], [0.5])
             data[camera_type] = image
-        elif 'gaze_pred' in camera_type and not g_conf.ATTENTION_AS_INPUT:
+        elif any(gaze_type in camera_type for gaze_type in ['gaze_pred', 'scout']) and not g_conf.ATTENTION_AS_INPUT:
             image = data[camera_type]
             image = cv2.resize(np.array(image), (3 * resize_attention[0], resize_attention[1]), interpolation=cv2.INTER_AREA)
             image = TF.to_tensor(image)
@@ -186,7 +187,11 @@ def ted_transform(data: dict, image_shape: 'tuple[int]', resize_attention: 'tupl
     return data
 
 
-def encode_directions_6(directions):
+def encode_directions_6(directions, shift_command: int = None):
+    if shift_command is not None:
+        directions = float(directions) + shift_command
+        directions = directions % 6 + 1.0
+    
     # TURN_LEFT
     if float(directions) == 1.0:
         return [1, 0, 0, 0, 0, 0]
@@ -208,7 +213,11 @@ def encode_directions_6(directions):
     else:
         raise ValueError("Unexpcted direction identified %s" % str(directions))
 
-def encode_directions_4(directions):
+def encode_directions_4(directions, shift_command: int = None):
+    if shift_command is not None:
+        directions = float(directions) + shift_command
+        directions = directions % 4 + 1.0
+    
     # TURN_LEFT
     if float(directions) == 1.0:
         return [1, 0, 0, 0]

network/loss.py
@@ -155,9 +155,11 @@ def Action_nospeed_L1_Attention_KL(params):
 
     # Attention loss
     eps = 1e-12  # For numerical stability
-    att_loss = params['variable_weights']['attention'] * F.kl_div((params['attention_output']+eps).log(),  # Transf. Encoder attention map (GAPn)
-                                                                  params['targets_attention'], # Ground truth attention map (virtual or human)
-                                                                  reduction='batchmean')
+    att_loss = params['variable_weights']['attention'] * F.kl_div(
+        (params['attention_output'] + eps).log(),  # Transf. Encoder attention map (GAPn)
+        params['targets_attention'], # Ground truth attention map (virtual or human)
+        reduction='batchmean'
+    )
 
     if g_conf.ACCELERATION_AS_ACTION:
         acceleration_loss = actions_loss_mat[:, 1] * params['variable_weights']['actions']['acceleration']

network/models/architectures/CIL_multiview/CIL_multiview.py
@@ -36,7 +36,7 @@ class CIL_multiview(nn.Module):
         else:
             self.resize_att_h, self.resize_att_w = self.res_out_h, self.res_out_w
         # Get the sequence length; TODO: this assumes all cameras have the same resolution!
-        self.sequence_length = len([c for c in g_conf.DATA_USED if 'rgb' in c]) * g_conf.ENCODER_INPUT_FRAMES_NUM * self.res_out_h * self.res_out_w
+        self.sequence_length = len([c for c in g_conf.DATA_USED if any(cam_type in c for cam_type in ['rgb', 'sekonix', 'conti'])]) * g_conf.ENCODER_INPUT_FRAMES_NUM * self.res_out_h * self.res_out_w
 
         if not g_conf.NO_ACT_TOKENS:
             # Add the STR and ACC tokens as parameters
@@ -197,7 +197,7 @@ class CIL_multiview(nn.Module):
     def forward(self, s, s_d, s_s, s_a=None):
         S = int(g_conf.ENCODER_INPUT_FRAMES_NUM)
         B = s_d[0].shape[0]
-        cam = len([c for c in g_conf.DATA_USED if 'rgb' in c])  # Number of cameras
+        cam = len([c for c in g_conf.DATA_USED if any(cam_type in c for cam_type in ['rgb', 'sekonix', 'conti'])])  # Number of cameras
 
         encoded_obs, resnet_inter = self.encode_observations(s, s_d, s_s, s_a)  # [B, S*cam*h*w + K + R, D]
 
@@ -219,7 +219,7 @@ class CIL_multiview(nn.Module):
                      attn_refinement: bool = False, attn_p2p_affinity: bool = False):
         S = int(g_conf.ENCODER_INPUT_FRAMES_NUM)
         B = s_d[0].shape[0]
-        cam = len([c for c in g_conf.DATA_USED if 'rgb' in c])  # Number of cameras
+        cam = len([c for c in g_conf.DATA_USED if any(cam_type in c for cam_type in ['rgb', 'sekonix', 'conti'])])  # Number of cameras
 
         encoded_obs, resnet_inter = self.encode_observations(s, s_d, s_s, s_a)  # [B, S*cam*h*w + K, D]
 